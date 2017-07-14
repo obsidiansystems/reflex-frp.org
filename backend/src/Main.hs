3fb0a1c829beeaf4577aaa1173310e5d94787c1c
Ishaq Sloan <ishaq.sloan@obsidian.systems>
2017-07-14 14:44:58 -0400

    Code Reviewed, added comments

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -23,31 +23,39 @@ import Common.Route --used for urlToRoute function & Route data type
 
 main :: IO ()
 main = do 
+  -- | get the ByteString of the statically rendered <head>
   theHead <- fmap snd $ renderStatic siteHead
+  -- | 
   withFocus . quickHttpServe $ rootHandler theHead 
 
+-- | Generates a MonadSnap object
 rootHandler :: ByteString -> Snap ()
 rootHandler theHead = route 
-  [ ("", assetHandler)
+  [ ("", assetHandler) -- ^ (empty ByteString, MonadSnap holding static assets)
   , ("", do r <- getRequest
             let mRoute = urlToRoute $ decodeUtf8 $ rqURI r
             case mRoute of
-              Nothing -> pass
-              Just rt -> serveStaticIndex $ def
+              Nothing -> pass -- ^ Don't handle given request
+              -- | handle request with updated content in the body
+              Just rt -> serveStaticIndex $ def 
                 & appConfig_initialHead .~ Just theHead
                 & appConfig_initialBody .~ Just (liftIO $ fmap snd $ renderStatic $ siteBody rt)
             )
-  , ("", appHandler theHead $ fmap snd $ renderStatic $ siteBody $ Route_Home)
+  , ("", appHandler theHead $ fmap snd $ renderStatic $ siteBody $ Route_Home) 
+  -- ^ when the context path is an empty string, direct to Home
   ]
 
 assetHandler :: Snap ()
 assetHandler = do
+  -- | get context path from MonadSnap object
   p <- getsRequest rqPathInfo
+  -- | if context path ends with ".html" or doesn't contain a '.' ...
   if ".html" `BS.isSuffixOf` p || BSC.all (/= '.') p
     then serveAssets "assets" "static"
-    --Serve static HTML files without redirecting
+    -- ^ ...Serve static HTML files without redirecting
     else serveAssets "assets" "static"
 
+-- | assign initialHead and initalBody to MonadSnap object
 appHandler :: ByteString -> IO ByteString -> Snap ()
 appHandler theHead theBody = serveApp "" $ def
   & appConfig_initialHead .~ Just theHead
