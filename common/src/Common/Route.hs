0c1dd6f205e80906f1e29a45c2633664e735dbd9
Ryan Trinkle <ryan.trinkle@gmail.com>
2017-10-20 15:52:26 -0400

    Update to latest focus; general cleanup

--- common/src/Common/Route.hs
+++ common/src/Common/Route.hs
@@ -5,11 +5,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE LambdaCase #-}
 
-
 module Common.Route where
 
 import Reflex.Dom
-import Reflex.Dom.Path
 
 import Data.Text (Text)
 import qualified Data.Map as Map
@@ -22,18 +20,18 @@ data Route = Route_Home | Route_Tutorials | Route_Examples | Route_Documentation
   deriving (Show, Eq, Ord)
 
 --  | instance of class from Frontend.Route
-instance IsPath Route where 
-  pathToText = routeToUrl 
+instance IsPath Route where
+  pathToText = routeToUrl
   textToPath = fromMaybe Route_Home . urlToRoute
 
-instance WebRoute Route where 
+instance WebRoute Route where
   -- | Outputs text to be appended to url
   routeToUrl :: Route -> Text
   routeToUrl r = case r of
    Route_Home -> "/home"
    Route_Tutorials -> "/tutorials"
    Route_Examples -> "/examples"
-   Route_Documentation -> "/documentation" 
+   Route_Documentation -> "/documentation"
    Route_FAQ -> "/faq"
 
   -- | Look up if a given url extension exists within a list of routes
@@ -46,7 +44,7 @@ instance WebRoute Route where
    Route_Home -> "Home"
    Route_Tutorials -> "Tutorials"
    Route_Examples -> "Examples"
-   Route_Documentation -> "Documentation" 
+   Route_Documentation -> "Documentation"
    Route_FAQ -> "Faq"
 
   routeToWidget :: DomBuilder t m => Route -> m ()
@@ -68,12 +66,12 @@ tutorials :: (DomBuilder t m) => m ()
 tutorials = elClass "div" "main" $ do
     elClass "h3" "title" $ text "Tutorials"
     el "ol" $ do
-      el "li" $ do 
+      el "li" $ do
         el "label" $ text "Installation: "
-        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-platform/blob/develop/README.md") $ text "setup-instructions" 
-      el "li" $ do 
+        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-platform/blob/develop/README.md") $ text "setup-instructions"
+      el "li" $ do
         el "label" $ text "Beginner Friendly Tutorial: "
-        elAttr "a" ("href" =: "https://github.com/hansroland/reflex-dom-inbits/blob/master/tutorial.md") $ text "reflex-dom-inbits" 
+        elAttr "a" ("href" =: "https://github.com/hansroland/reflex-dom-inbits/blob/master/tutorial.md") $ text "reflex-dom-inbits"
 
 
 
@@ -81,24 +79,24 @@ examples :: (DomBuilder t m) => m ()
 examples = elClass "div" "main" $ do
      elClass "h3" "title" $ text "Check Out Some Example Code"
      el "ul" $ do
-      el "li" $ do 
+      el "li" $ do
         el "label" $ text "Basic ToDo List: "
-        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-examples/blob/master/BasicTodo/BasicTodo.hs") $ text "See Code Here" 
-      el "li" $ do 
+        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-examples/blob/master/BasicTodo/BasicTodo.hs") $ text "See Code Here"
+      el "li" $ do
         el "label" $ text "JSON API - NASA Pic of the Day: "
-        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-examples/blob/master/nasa-pod/workshop.hs") $ text "See Code Here" 
+        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-examples/blob/master/nasa-pod/workshop.hs") $ text "See Code Here"
 
 
 documentation :: (DomBuilder t m) => m ()
 documentation = elClass "div" "main" $ do
     elClass "h3" "title" $ text "Refreshing Reflex Documentation"
     el "ul" $ do
-      el "li" $ do 
+      el "li" $ do
         el "label" $ text "Reflex Basic Documentation: "
-        elAttr "a" ("href" =: "http://reflex-frp.readthedocs.io/en/latest/architecture.html#overview-of-reflex-basics") $ text "View Here" 
-      el "li" $ do 
-        el "label" $ text "Quick Reference: " 
-        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-dom/blob/develop/Quickref.md") $ text "View Here" 
+        elAttr "a" ("href" =: "http://reflex-frp.readthedocs.io/en/latest/architecture.html#overview-of-reflex-basics") $ text "View Here"
+      el "li" $ do
+        el "label" $ text "Quick Reference: "
+        elAttr "a" ("href" =: "https://github.com/reflex-frp/reflex-dom/blob/develop/Quickref.md") $ text "View Here"
 
 
 faq :: (DomBuilder t m) => m ()
--- frontend/src/Frontend/App.hs
+++ frontend/src/Frontend/App.hs
@@ -8,18 +8,16 @@ module Frontend.App where
 
 import Reflex
 import Reflex.Dom
-import Reflex.Dom.Path
 
 import qualified Data.Map as Map
 import Data.Monoid
 import Data.Map (Map)
 import Data.Text (Text)
-import Common.Route -- ^ used for navBar's Route data type 
+import Common.Route -- ^ used for navBar's Route data type
 import Control.Monad.Fix
 import Control.Monad
 
 import Obelisk.FontAwesome as FA
-import Web.FontAwesomeType
 import Obelisk.Haskpress.Frontend.Kiss
 
 ------------------- <head></head> ----------------------------------------
@@ -32,7 +30,7 @@ siteHead = do
   fontAwesomeCDN                      -- ^ link MaxCDN FontAwesome
   -- | add various favIcon links
   faviconLinker "icon" "image/png" "16x16" "img/favicon-16x16.png"
-  faviconLinker "icon" "image/png" "32x32" "img/favicon-32x32.png" 
+  faviconLinker "icon" "image/png" "32x32" "img/favicon-32x32.png"
   faviconLinker "apple-touch-icon" "/" "57x57" "img/apple-touch-icon-57x57.png"
   faviconLinker "apple-touch-icon" "/" "60x60" "img/apple-touch-icon-60x60.png"
   faviconLinker "apple-touch-icon" "/" "72x72" "img/apple-touch-icon-72x72.png"
@@ -51,16 +49,16 @@ siteHead = do
 siteBody :: (DomBuilder t m, MonadHold t m, MonadFix m, TriggerEvent t m, PostBuild t m
             , PerformEvent t m, Prerender x m, WebRoute Route, IsPath Route, Ord Route)
             => Route -> m ()
-siteBody initRoute = do 
-  bodyGen siteLogo siteRoutes initRoute  
-  elClass "div" "main" $ do 
+siteBody initRoute = do
+  bodyGen siteLogo siteRoutes initRoute
+  elClass "div" "main" $ do
     el "p" $ text "Check us out on Hackage or join the community IRC chat!"
     forM_ links $ \pair -> do
       elAttr "a" ("href" =: (snd pair)) $ text (fst pair)
       el "br" $ return ()
   el "br" blank
 
-  -- | Place Font Awesome Icons in footer <div> 
+  -- | Place Font Awesome Icons in footer <div>
   elClass "div" "footer" $ do
     elAttr "a" rdirTwitter $ do
       FA.faIcon FaTwitter def
@@ -69,11 +67,11 @@ siteBody initRoute = do
     elAttr "a" rdirReddit $ do
       FA.faIcon FaReddit def
 
-  where 
+  where
     siteLogo = "img/REFLEX.png"
     siteRoutes = [ Route_Home
                  , Route_Tutorials
-                 , Route_Examples 
+                 , Route_Examples
                  , Route_Documentation
                  , Route_FAQ ]
     links = [ ("Hackage", "https://hackage.haskell.org/package/reflex")
@@ -95,8 +93,8 @@ styleSheet myLink = elAttr "link" (Map.fromList [
 -- The 4 arguments are as follows: rel type size href
 -- turn the second argument into a Maybe Text
 faviconLinker :: DomBuilder t m => Text -> Text -> Text -> Text -> m ()
-faviconLinker r t s h = elAttr "link" attribs blank 
-    where 
+faviconLinker r t s h = elAttr "link" attribs blank
+    where
       attribs = "rel" =: r
              <> "type" =: t
              <> "size" =: s
@@ -104,20 +102,20 @@ faviconLinker r t s h = elAttr "link" attribs blank
 
 ----------------------Element Attributes------------------------------
 metaDesc :: Map Text Text
-metaDesc = "name" =: "description" 
+metaDesc = "name" =: "description"
         <> "content" =: "Reflex Functional Reactive Programming"
 
 metaKeywords :: Map Text Text
 metaKeywords = "name" =: "keywords"
-            <> "content" =: "reflex, reflex frp, functional reactive programming, haskell, framework, reflex dom" 
+            <> "content" =: "reflex, reflex frp, functional reactive programming, haskell, framework, reflex dom"
 
 viewport :: Map Text Text
 viewport = "name" =: "viewport"
         <> "content" =: "width=device-width, initial-scale=1"
 
 logo :: Map Text Text
-logo = "class" =: "logo" 
-        <> "src" =: "img/REFLEX.png" 
+logo = "class" =: "logo"
+        <> "src" =: "img/REFLEX.png"
 
 icon :: Map Text Text
 icon = "rel" =: "shortcut icon"
@@ -132,4 +130,3 @@ rdirGithub = "href" =: "http://github.com/reflex-frp"
 rdirReddit :: Map Text Text
 rdirReddit = "href" =: "http://reddit.com/r/reflexfrp"
            <> "title" =: "reddit"
-
